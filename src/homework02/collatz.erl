408a400326d42260f61cef2ef3c207612acab478
Serhii <serhiimelnik@gmail.com>
2015-08-28 09:53:40 +0300

    fix collatz module

--- src/homework02/collatz.erl
+++ src/homework02/collatz.erl
@@ -3,9 +3,13 @@
 
 -export([collatz/1]).
 
-collatz(N) -> collatz(N, 0, []).
+collatz(N) -> collatz(N, 0, [N]).
 
-collatz(1, C, Acc) -> io:format("~w~n Number of steps - ~w~n", [lists:reverse(Acc), C]);
-collatz(N, C, Acc) when N rem 2 =:= 0 -> collatz(N, C + 1, [N / 2 |Acc]);
-collatz(N, C, Acc) when N rem 2 =/= 0 -> collatz(N, C + 1, [N * 3 + 1 |Acc]);
+collatz(1, C, Acc) -> io:format("~w~nNumber of steps - ~w~n", [lists:reverse(Acc), C]);
+collatz(N, C, Acc) when N rem 2 =:= 0 ->
+  R = N div 2,
+  collatz(R, C + 1, [R|Acc]);
+collatz(N, C, Acc) when N rem 2 =:= 1 ->
+  R = N * 3 + 1,
+  collatz(R, C + 1, [R|Acc]);
 collatz(_, _, Acc) -> Acc.
