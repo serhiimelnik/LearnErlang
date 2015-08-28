4b2f07eabf999e8ff42100104a47517047242253
Serhii <serhiimelnik@gmail.com>
2015-08-28 09:57:01 +0300

    refactor collatz

--- src/homework02/collatz.erl
+++ src/homework02/collatz.erl
@@ -3,7 +3,7 @@
 
 -export([collatz/1]).
 
-collatz(N) -> collatz(N, 0, [N]).
+collatz(N) when is_integer(N), N > 0 -> collatz(N, 0, [N]).
 
 collatz(1, C, Acc) -> io:format("~w~nNumber of steps - ~w~n", [lists:reverse(Acc), C]);
 collatz(N, C, Acc) when N rem 2 =:= 0 ->
@@ -11,5 +11,4 @@ collatz(N, C, Acc) when N rem 2 =:= 0 ->
   collatz(R, C + 1, [R|Acc]);
 collatz(N, C, Acc) when N rem 2 =:= 1 ->
   R = N * 3 + 1,
-  collatz(R, C + 1, [R|Acc]);
-collatz(_, _, Acc) -> Acc.
+  collatz(R, C + 1, [R|Acc]).
