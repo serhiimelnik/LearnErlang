fe20cc03197fe1f3045efdbaded1b40c52706db6
Serhii <serhiimelnik@gmail.com>
2015-08-28 20:48:17 +0300

    zip_with

---
+++ src/homework04/zip_with.erl
@@ -0,0 +1,17 @@
+-module(zip_with).
+-author("Serhii Melnyk").
+
+-export([zip_with/3]).
+
+%% zip_with(F, [H1|T1], [H2|T2]) ->
+%%   [F(H1,H2)].
+zip_with(F, L1, L2) -> lists:reverse(zip_with_rec(F, L1, L2, [])).
+
+zip_with_rec(_F, [], [], Acc) -> Acc;
+zip_with_rec(_F, [], _, Acc) -> Acc;
+zip_with_rec(_F, _, [], Acc) -> Acc;
+zip_with_rec(F, [H1|T1], [H2|T2], Acc) ->
+  zip_with_rec(F, T1, T2, [F(H1,H2)|Acc]).
+
+
+%% use foldl
