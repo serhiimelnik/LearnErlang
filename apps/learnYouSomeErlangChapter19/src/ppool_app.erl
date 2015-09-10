27a59939fa20d0d4038aff0aa07247a8a7c6eff4
Serhii Melnik <serhiimelnik@gmail.com>
2015-09-10 22:57:11 +0300

    finish learnYouSomeErlangChapter19

--- apps/learnYouSomeErlangChapter19/src/ppool_app.erl
+++ apps/learnYouSomeErlangChapter19/src/ppool_app.erl
@@ -1,10 +1,11 @@
 %%% API module for the pool
--module(ppool).
+-module(ppool_app).
 -behaviour(application).
 -export([start/2, stop/1, start_pool/3,
   run/2, sync_queue/2, async_queue/2, stop_pool/1]).
 
 start(normal, _Args) ->
+  io:format("done"),
   ppool_supersup:start_link().
 
 stop(_State) ->
